d37f169fe7fe4dab10ed9033097067a2863b5f2f
Tom Stahlberg <tom.stahlberg@jakkit.de>
2023-04-12 01:44:11 +0200

    Initial commit

---
+++ src/main/java/de/tomstahlberg/goldenskyblock/Goldenskyblock.java
@@ -0,0 +1,17 @@
+package de.tomstahlberg.goldenskyblock;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Goldenskyblock extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
